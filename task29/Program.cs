b71ec89d77dbfed101e3452ceab235722cbc3c97
Elena Petrova <elena.i.11@yandex.ru>
2022-08-20 23:47:09 +0400

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами из [a, b) и выводит их на экран.
+
+void RandArray ()
+{
+    int [] arr = new int [arr.Length];
+
+    for (int i=0; i<arr.Length; i++);
+    {
+        arr [i] = new Random().Next[1,0];
+        Console.write("{0} ", arr[i]);
+      }
+      Console.WriteLine();
+}
+
+RandArray ();
