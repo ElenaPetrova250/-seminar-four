079f76f409a7e4349c68da94e2b7b624e3711944
Elena Petrova <elena.i.11@yandex.ru>
2022-08-20 22:49:39 +0400

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = int.Parse(Console.ReadLine());
+
+int SumNumber (int number)
+{
+    int count = Convert.ToString(number).Length;
+    count = 0;
+    result = 0;
+    {
+        for (int i = 0; i < count; i++)
+        count = number - number % 10;
+        result +=
+        number = number/10;
+    }
+    return result;
+}
+int SumNumber = SumNumber (number);
+Console.WriteLine("Сумма цифр с учетом: " + SumNumber);
